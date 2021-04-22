1156ecf4c28c4c77f87d8468af9f4d165f4e9cef
HMSteve <HMSteve@users.noreply.github.com>
2021-04-22 12:18:11 +0200

    Update Sens_VEML6030.h

--- sensors/Sens_VEML6030.h
+++ sensors/Sens_VEML6030.h
@@ -10,8 +10,8 @@
 #include <Wire.h>
 #include <SparkFun_VEML6030_Ambient_Light_Sensor.h>
 
-#define GAIN 0.125
-#define INTEGTIME 100
+#define GAIN 2
+#define INTEGTIME 400
 
 namespace as {
 
@@ -35,10 +35,14 @@ public:
     DPRINT(F("VEML6030 "));
     if (_present) {
       DPRINTLN(F("OK"));
+      _veml6030.setPowSavMode(4);
+      _veml6030.enablePowSave();
       _veml6030.setGain(GAIN);
       _veml6030.setIntegTime(INTEGTIME);
-      DPRINT("VEML6030 Gain             : ");DDECLN(_veml6030.readGain());
-      DPRINT("VEML6030 Integration Time : ");DDECLN(_veml6030.readIntegTime());
+      DPRINT("VEML6030 Gain               : ");DDECLN(_veml6030.readGain());
+      DPRINT("VEML6030 Integration Time   : ");DDECLN(_veml6030.readIntegTime());
+      DPRINT("VEML6030 Power Save Enabled : ");DDECLN(_veml6030.readPowSavEnabled());
+      DPRINT("VEML6030 Power Save Mode    : ");DDECLN(_veml6030.readPowSavMode());
     } else {
       DPRINTLN(F("ERROR"));
     }
@@ -47,11 +51,11 @@ public:
 
   bool measure (__attribute__((unused)) bool async=false) {
     if( present() == true ) {
-      _veml6030.powerOn();
-      delay(150);
+      //_veml6030.powerOn();
+      //delay(150);
       _illuminance = _veml6030.readLight();
       DPRINT("VEML6030 Illumination (Lux)   : "); DDECLN(_illuminance);
-      _veml6030.shutDown();
+      //_veml6030.shutDown();
       return true;
     }
     return false;
